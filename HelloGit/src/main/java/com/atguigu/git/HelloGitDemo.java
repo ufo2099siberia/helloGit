d62f25a4756702a4a5d934cd8e5c2aaf985fc55f
tom_glb <goodMorning_glb@atguigu.com>
2020-11-30 20:30:38 +0800

    分支添加了内容

--- HelloGit/src/main/java/com/atguigu/git/HelloGitDemo.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGitDemo.java
@@ -7,6 +7,7 @@ public class HelloGitDemo {
         System.out.println("HelloGit!");
         System.out.println("HelloGit!更新1");
         System.out.println("HelloGit!创建分支");
+        System.out.println("HelloGit!分支添加了内容");
 
     }
 
