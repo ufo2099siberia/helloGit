7677c9ab711986100d80c3b40c86d86f3eddae94
tom_glb <goodMorning_glb@atguigu.com>
2020-12-01 13:54:22 +0800

    格式化一波

--- HelloGit/src/main/java/com/atguigu/git/HelloGitDemo.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGitDemo.java
@@ -3,22 +3,16 @@ package com.atguigu.git;
 public class HelloGitDemo {
 
     public static void main(String[] args) {
-
         System.out.println("HelloGit!");
         System.out.println("HelloGit!更新1");
         System.out.println("HelloGit!创建分支");
         System.out.println("HelloGit!主干添加内容");
         System.out.println("HelloGit!分支添加了内容");
-         System.out.println("HelloGit!在线更新编辑发牌");
-
-         System.out.println("HelloGit!克隆下来的项目");
+        System.out.println("HelloGit!克隆下来的项目");
         System.out.println("merge or rebase");
         System.out.println("测试merge还是rebase");
-        
         System.out.println("different between rebase and merge!");
-
         System.out.println("这次需要测试直接rebase有什么变化");
-
     }
 
 }
